8cd549cedd2c3ed0f44046535c5f4de039be9b5d
darwinsubramaniam_Development_PC1 <darwinsubramaniam@googlemail.com>
2016-03-05 15:39:53 +0100

    Set a proper directory for Android developlent

---
+++ LetzEat/Android/LetzEat/Login/src/main/java/udworldnet/com/letzeat/ServerSetting.java
@@ -0,0 +1,11 @@
+package udworldnet.com.letzeat;
+
+/**
+ * Created by darwin on 05/03/16.
+ * This provide the value used to communicate with the server Backend
+ */
+public class ServerSetting {
+    public static final String APP_ID = "CB2E81EF-B683-0D21-FF75-1BC45CC57500";
+    public static final String APP_KEY = "A8A6108B-DABE-BB22-FF6E-67ECF8E25000";
+    public static final String VERSION = "v1";
+}
